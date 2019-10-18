7d6123f08caa56ee93295115e6a9e438eeae30ac
Kyle Lacy <kylelacy+git@pm.me>
2019-10-17 23:09:03 -0700

    Evaluate JavaScript from stdin using `ducc`

--- brioche/src/main.rs
+++ brioche/src/main.rs
@@ -1,3 +1,22 @@
+use std::io::{self, Read as _};
+use serde_json;
+use ducc;
+use ducc_serde;
+
 fn main() {
-    println!("Hello, world!");
+    let mut js = String::new();
+
+    let stdin = io::stdin();
+    let mut stdin = stdin.lock();
+    stdin.read_to_string(&mut js).expect("Failed to read JS from stdin");
+
+    let ducc = ducc::Ducc::new();
+    let result: ducc::Value = ducc.exec(
+        &js,
+        Some("stdin"),
+        ducc::ExecSettings::default()
+    ).expect("Execution failed");
+    let result: serde_json::Value = ducc_serde::from_value(result).unwrap();
+
+    println!("{:?}", result);
 }
