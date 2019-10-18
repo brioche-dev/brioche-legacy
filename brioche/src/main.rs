8fc2470783d0f09959918ff45dd8d696d4fa4865
Kyle Lacy <kylelacy+git@pm.me>
2019-10-17 23:24:42 -0700

    Refactor JS evaluation with new `JsExec` type

--- brioche/src/main.rs
+++ brioche/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(inner_deref)]
+
 use std::path;
 use std::fs;
 use serde_json;
@@ -10,17 +12,35 @@ struct Opt {
     recipe: path::PathBuf,
 }
 
+struct JsExec {
+    ducc: ducc::Ducc,
+}
+
+impl JsExec {
+    fn new() -> Self {
+        let ducc = ducc::Ducc::new();
+
+        JsExec { ducc }
+    }
+
+    fn eval(&self, js: &str, name: Option<&str>) -> serde_json::Value {
+        let result: ducc::Value = self.ducc.exec(
+            js,
+            name,
+            ducc::ExecSettings::default()
+        ).expect("JS execution failed");
+        let result: serde_json::Value = ducc_serde::from_value(result).unwrap();
+
+        result
+    }
+}
+
 fn main() {
     let opt = Opt::from_args();
     let js = fs::read_to_string(&opt.recipe).expect("Failed to read JS file");
 
-    let ducc = ducc::Ducc::new();
-    let result: ducc::Value = ducc.exec(
-        &js,
-        Some("stdin"),
-        ducc::ExecSettings::default()
-    ).expect("Execution failed");
-    let result: serde_json::Value = ducc_serde::from_value(result).unwrap();
+    let js_exec = JsExec::new();
+    let result = js_exec.eval(&js, opt.recipe.to_str().as_deref());
 
     println!("{:?}", result);
 }
