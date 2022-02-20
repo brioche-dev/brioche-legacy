ec651cacb1bf99e4850fa72561c51978f44df2bf
Kyle Lacy <kylelacy@kyle.space>
2022-02-19 23:31:25 -0800

    Add initial example `hello` recipe

---
+++ example-repo/hello/brioche.js
@@ -0,0 +1,33 @@
+// import { sh } from "@brioche-dev/v0";
+
+function sh(template, ...args) {
+    if (template.length > 1 || args.length > 0) {
+        throw new Error("Cannot interpolate values");
+    }
+
+    return {
+        shell: "sh",
+        script: template[0],
+        envVars: {},
+    };
+}
+
+const VERSION = "2.12";
+
+export const recipe = {
+    options: {},
+    definition: () => ({
+        name: "hello",
+        version: VERSION,
+        source: {
+            git: "https://git.savannah.gnu.org/cgit/hello.git",
+            ref: `v${VERSION}`,
+        },
+        dependencies: {},
+        build: sh`
+            ./bootstrap
+            make
+            make install
+        `,
+    }),
+};
