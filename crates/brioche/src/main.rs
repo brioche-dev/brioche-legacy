b70c52290a26756c34d490c01235cb55a173ab40
Kyle Lacy <kylelacy@kyle.space>
2022-02-19 18:26:20 -0800

    Set up basic command-line parsing and error reporting

--- crates/brioche/src/main.rs
+++ crates/brioche/src/main.rs
@@ -1,3 +1,27 @@
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+enum Opt {
+    Build { path: PathBuf },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = run();
+    match result {
+        Ok(()) => {}
+        Err(error) => {
+            eprintln!("{:#}", error);
+            std::process::exit(1);
+        }
+    }
+}
+
+fn run() -> anyhow::Result<()> {
+    let opt = Opt::from_args();
+
+    println!("{:?}", opt);
+
+    Ok(())
 }
