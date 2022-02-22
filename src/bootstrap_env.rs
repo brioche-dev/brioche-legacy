d73cb5bf56124629a4416426102973b068bfc2b6
Kyle Lacy <kylelacy@kyle.space>
2022-02-21 18:36:16 -0800

    Refactor `State.download` with new `ContentRequest` type
    The SHA hash of the download file is now also optional

--- src/bootstrap_env.rs
+++ src/bootstrap_env.rs
@@ -32,10 +32,9 @@ impl BootstrapEnv {
         let overlay_dir = work_dir.join("overlay");
         fs::create_dir_all(&overlay_dir).await?;
 
-        let mut alpine_tar_gz = state.download(
-            "https://dl-cdn.alpinelinux.org/alpine/v3.15/releases/x86_64/alpine-minirootfs-3.15.0-x86_64.tar.gz".parse()?,
-            &hex!("ec7ec80a96500f13c189a6125f2dbe8600ef593b87fc4670fe959dc02db727a2"),
-        ).await?;
+        let alpine_tar_gz_req = crate::state::ContentRequest::new("https://dl-cdn.alpinelinux.org/alpine/v3.15/releases/x86_64/alpine-minirootfs-3.15.0-x86_64.tar.gz".parse()?)
+            .hash(hex!("ec7ec80a96500f13c189a6125f2dbe8600ef593b87fc4670fe959dc02db727a2"));
+        let mut alpine_tar_gz = state.download(&alpine_tar_gz_req).await?;
         let alpine_root_dir = state.unpack(&mut alpine_tar_gz).await?;
 
         println!(
--- src/state.rs
+++ src/state.rs
@@ -49,13 +49,21 @@ impl State {
         Ok(work_dir)
     }
 
-    pub async fn download(&self, url: Url, sha_hash: &[u8; 32]) -> anyhow::Result<ContentFile> {
+    pub async fn get_existing_content_file(&self, req: &ContentRequest) -> Option<ContentFile> {
+        let content_hash = req.content_hash.as_ref()?;
+
+        let file_path = self.downloads_dir.join(hex::encode(&content_hash));
+        let file = fs::File::open(&file_path).await.ok()?;
+
+        Some(ContentFile { file })
+    }
+
+    pub async fn download(&self, req: &ContentRequest) -> anyhow::Result<ContentFile> {
         use sha2::Digest as _;
 
-        let final_file_path = self.downloads_dir.join(hex::encode(&sha_hash));
-        let final_file = fs::File::open(&final_file_path).await;
-        if let Ok(file) = final_file {
-            return Ok(ContentFile { file });
+        let existing_file = self.get_existing_content_file(&req).await;
+        if let Some(existing) = existing_file {
+            return Ok(existing);
         };
 
         let download_id = Uuid::new_v4();
@@ -67,7 +75,7 @@ impl State {
             .open(&temp_file_path)
             .await?;
 
-        let response = reqwest::get(url.clone()).await?;
+        let response = reqwest::get(req.url.clone()).await?;
         dbg!(response.error_for_status_ref())?;
         let mut file_hash = sha2::Sha256::new();
 
@@ -78,29 +86,36 @@ impl State {
             file_hash.update(&chunk);
         }
 
-        let file_hash = file_hash.finalize();
-        if &*file_hash == &*sha_hash {
-            let rename_result = fs::rename(&temp_file_path, &final_file_path).await;
-            match rename_result {
-                Ok(()) => {
-                    println!("Downloaded URL {} -> {}", url, final_file_path.display());
-                }
-                Err(error) => {
-                    eprintln!(
-                        "Downloaded URL {} -> {} (failed to rename: {})",
-                        url,
-                        temp_file_path.display(),
-                        error
-                    );
-                }
+        let downloaded_hash = file_hash.finalize();
+        if let Some(expected_hash) = req.content_hash {
+            if &expected_hash != &*downloaded_hash {
+                anyhow::bail!(
+                    "File hash did not match for {} (expected {}, got {})",
+                    req.url,
+                    hex::encode(expected_hash),
+                    hex::encode(downloaded_hash),
+                );
             }
-        } else {
-            anyhow::bail!(
-                "File hash did not match for {} (expected {}, got {})",
-                url,
-                hex::encode(sha_hash),
-                hex::encode(file_hash),
-            );
+        };
+
+        let final_file_path = self.downloads_dir.join(hex::encode(&downloaded_hash));
+        let rename_result = fs::rename(&temp_file_path, &final_file_path).await;
+        match rename_result {
+            Ok(()) => {
+                println!(
+                    "Downloaded URL {} -> {}",
+                    req.url,
+                    final_file_path.display()
+                );
+            }
+            Err(error) => {
+                eprintln!(
+                    "Downloaded URL {} -> {} (failed to rename: {})",
+                    req.url,
+                    temp_file_path.display(),
+                    error
+                );
+            }
         }
 
         download_file.seek(SeekFrom::Start(0)).await?;
@@ -123,4 +138,23 @@ impl State {
 
 pub struct ContentFile {
     file: tokio::fs::File,
+}
+
+pub struct ContentRequest {
+    url: Url,
+    content_hash: Option<[u8; 32]>,
+}
+
+impl ContentRequest {
+    pub fn new(url: Url) -> Self {
+        Self {
+            url,
+            content_hash: None,
+        }
+    }
+
+    pub fn hash(mut self, sha_hash: [u8; 32]) -> Self {
+        self.content_hash = Some(sha_hash);
+        self
+    }
 }
