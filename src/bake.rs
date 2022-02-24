d5da415cf851de67822c66be8ced0792504fc44a
Kyle Lacy <kylelacy@kyle.space>
2022-02-24 01:22:50 -0800

    Remove `$BRIOCHE_BOOTSTRAP_SYSROOT` env var when baking recipes

--- src/bake.rs
+++ src/bake.rs
@@ -95,7 +95,6 @@ pub async fn get_baked_recipe(
     command.current_dir(bootstrap_env.container_source_path());
     command.env("BRIOCHE_PREFIX", &recipe_prefix.container_path);
     command.env("BRIOCHE_BOOTSTRAP_TARGET", bootstrap_env.bootstrap_target());
-    command.env("BRIOCHE_BOOTSTRAP_SYSROOT", &recipe_prefix.container_path);
 
     let mut child = bootstrap_env.spawn(&command)?;
     let child_stdin = child.take_stdin();
